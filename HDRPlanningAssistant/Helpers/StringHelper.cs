5383dc2ac16ff381525b3eab0d954ef90c5da971
esimiele <simiele77@gmail.com>
2023-11-10 14:32:15 -0600

    Made some progress in converting to exe. Built the UI and added dummy functions to the backend.
    Need to do a bunch of refactoring

---
+++ HDRPlanningAssistant/Helpers/StringHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HDRPlanningAssistant.Helpers
+{
+    public static class StringHelper
+    {
+        public static string cropLine(string line, string cropChar) 
+        { 
+            return line.Substring(line.IndexOf(cropChar) + 1, line.Length - line.IndexOf(cropChar) - 1); 
+        }
+    }
+}
---
+++ HDRPlanningAssistant/MainWindow.xaml.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HDRPlanningAssistant
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow(string[] args)
+        {
+            InitializeComponent();
+            InitializeScript(args);
+        }
+
+        private void InitializeScript(string[] args)
+        {
+            throw new NotImplementedException();
+        }
+
+        #region help docs and shortcuts
+        private void OpenHelpClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ShowShortcutsClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+        #endregion
+
+        private void EBRTdosePerFxTBTextChanged(object sender, TextChangedEventArgs e)
+        {
+            
+        }
+        private void EBRTnumFxTBTextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        #region request DVH metrics
+        private void AddDVHMetricClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void AddDefaultMetricsClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ClearResultsClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+        #endregion
+
+        #region Calculate metrics
+        private void CalculateMetricsClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+        #endregion
+
+        private void ShowIdealDoses(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ShowMDAWindow(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        #region write results
+        private void WriteResultsTextClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void WriteResultsExcelClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+        #endregion
+
+        #region second dose calc
+        private void RunSecondCheckClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+        #endregion
+    }
+}
